4a298852e1183356c9b0e57cdef0ad05d027c510
Kazuhiro Osawa <yappo@shibuya.pl>
2011-01-28 19:03:10 +0900

    initial commit

---
+++ lib/Module/Install/HTML5Manifest.pm
@@ -0,0 +1,32 @@
+package Module::Install::HTML5Manifest;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Module::Install::HTML5Manifest -
+
+=head1 SYNOPSIS
+
+  use Module::Install::HTML5Manifest;
+
+=head1 DESCRIPTION
+
+Module::Install::HTML5Manifest is
+
+=head1 AUTHOR
+
+Kazuhiro Osawa E<lt>yappo {at} shibuya {dot} plE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
